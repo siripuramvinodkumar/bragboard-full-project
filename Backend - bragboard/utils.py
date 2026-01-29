67b359655a87298e519983dcf2aa5389f559de17
SIRIPURAM VINOD KUMAR <siripuramvinodkumar333@gmail.com>
2026-01-29 10:13:29 +0530

    Update utils.py

--- Backend - bragboard/utils.py
+++ Backend - bragboard/utils.py
@@ -1,9 +1,15 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
 
-def hash_password(password: str):
-    return pwd_context.hash(password[:72])
+# bcrypt max password length = 72 bytes
+def hash_password(password: str) -> str:
+    safe_password = password[:72]
+    return pwd_context.hash(safe_password)
 
-def verify_password(plain_password: str, hashed_password: str):
-    return pwd_context.verify(plain_password[:72], hashed_password)
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    safe_password = plain_password[:72]
+    return pwd_context.verify(safe_password, hashed_password)
